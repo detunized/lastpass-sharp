d834a4268026e6a7854c51ad059fdc887e700b15
detunized <detunized@gmail.com>
2013-06-22 16:48:22 +0200

    Store test data in more compact way

--- test/PBKDF2Test.cs
+++ test/PBKDF2Test.cs
@@ -2,108 +2,47 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using LastPass;
+using NUnit.Framework;
 
 namespace LastPass.Test
 {
-    using NUnit.Framework;
-
     [TestFixture]
     class PBKDF2Test
     {
         private struct TestData
         {
-            public string password;
-            public string salt;
-            public int iterationCount;
-            public byte[] expected;
+            public TestData(string password, string salt, int iterationCount, string expected)
+            {
+                Password = password;
+                Salt = salt;
+                IterationCount = iterationCount;
+                Expected = expected;
+            }
+
+            public readonly string Password;
+            public readonly string Salt;
+            public readonly int IterationCount;
+            public readonly string Expected;
         };
 
         // Test data for PBKDF2 HMAC-SHA1 is from http://tools.ietf.org/html/rfc6070
-        private TestData[] testDataSHA1 = new TestData[]
+        private TestData[] testDataSHA1 =
         {
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 1,
-                expected = new byte[] {0x0c, 0x60, 0xc8, 0x0f, 0x96, 0x1f, 0x0e, 0x71, 0xf3, 0xa9, 0xb5, 0x24, 0xaf, 0x60, 0x12, 0x06, 0x2f, 0xe0, 0x37, 0xa6}
-            },
-
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 2,
-                expected = new byte[] {0xea, 0x6c, 0x01, 0x4d, 0xc7, 0x2d, 0x6f, 0x8c, 0xcd, 0x1e, 0xd9, 0x2a, 0xce, 0x1d, 0x41, 0xf0, 0xd8, 0xde, 0x89, 0x57}
-            },
-
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 4096,
-                expected = new byte[] {0x4b, 0x00, 0x79, 0x01, 0xb7, 0x65, 0x48, 0x9a, 0xbe, 0xad, 0x49, 0xd9, 0x26, 0xf7, 0x21, 0xd0, 0x65, 0xa4, 0x29, 0xc1}
-            },
-
-            new TestData
-            {
-                password = "passwordPASSWORDpassword",
-                salt = "saltSALTsaltSALTsaltSALTsaltSALTsalt",
-                iterationCount = 4096,
-                expected = new byte[] {0x3d, 0x2e, 0xec, 0x4f, 0xe4, 0x1c, 0x84, 0x9b, 0x80, 0xc8, 0xd8, 0x36, 0x62, 0xc0, 0xe4, 0x4a, 0x8b, 0x29, 0x1a, 0x96, 0x4c, 0xf2, 0xf0, 0x70, 0x38}
-            },
-
-            new TestData
-            {
-                password = "pass\0word",
-                salt = "sa\0lt",
-                iterationCount = 4096,
-                expected = new byte[] {0x56, 0xfa, 0x6a, 0xa7, 0x55, 0x48, 0x09, 0x9d, 0xcc, 0x37, 0xd7, 0xf0, 0x34, 0x25, 0xe0, 0xc3}
-            },
+            new TestData("password", "salt", 1, "DGDID5YfDnHzqbUkr2ASBi/gN6Y="),
+            new TestData("password", "salt", 2, "6mwBTcctb4zNHtkqzh1B8NjeiVc="),
+            new TestData("password", "salt", 4096, "SwB5AbdlSJq+rUnZJvch0GWkKcE="),
+            new TestData("passwordPASSWORDpassword", "saltSALTsaltSALTsaltSALTsaltSALTsalt", 4096, "PS7sT+QchJuAyNg2YsDkSospGpZM8vBwOA=="),
+            new TestData("pass\0word", "sa\0lt", 4096, "Vvpqp1VICZ3MN9fwNCXgww=="),
         };
 
         // Test data for PBKDF2 HMAC-SHA256 is from http://stackoverflow.com/a/5136918/362938
-        private TestData[] testDataSHA256 = new TestData[]
+        private TestData[] testDataSHA256 =
         {
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 1,
-                expected = new byte[] {0x12, 0x0f, 0xb6, 0xcf, 0xfc, 0xf8, 0xb3, 0x2c, 0x43, 0xe7, 0x22, 0x52, 0x56, 0xc4, 0xf8, 0x37, 0xa8, 0x65, 0x48, 0xc9, 0x2c, 0xcc, 0x35, 0x48, 0x08, 0x05, 0x98, 0x7c, 0xb7, 0x0b, 0xe1, 0x7b}
-            },
-
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 2,
-                expected = new byte[] {0xae, 0x4d, 0x0c, 0x95, 0xaf, 0x6b, 0x46, 0xd3, 0x2d, 0x0a, 0xdf, 0xf9, 0x28, 0xf0, 0x6d, 0xd0, 0x2a, 0x30, 0x3f, 0x8e, 0xf3, 0xc2, 0x51, 0xdf, 0xd6, 0xe2, 0xd8, 0x5a, 0x95, 0x47, 0x4c, 0x43}
-            },
-
-            new TestData
-            {
-                password = "password",
-                salt = "salt",
-                iterationCount = 4096,
-                expected = new byte[] {0xc5, 0xe4, 0x78, 0xd5, 0x92, 0x88, 0xc8, 0x41, 0xaa, 0x53, 0x0d, 0xb6, 0x84, 0x5c, 0x4c, 0x8d, 0x96, 0x28, 0x93, 0xa0, 0x01, 0xce, 0x4e, 0x11, 0xa4, 0x96, 0x38, 0x73, 0xaa, 0x98, 0x13, 0x4a}
-            },
-
-            new TestData
-            {
-                password = "passwordPASSWORDpassword",
-                salt = "saltSALTsaltSALTsaltSALTsaltSALTsalt",
-                iterationCount = 4096,
-                expected = new byte[] {0x34, 0x8c, 0x89, 0xdb, 0xcb, 0xd3, 0x2b, 0x2f, 0x32, 0xd8, 0x14, 0xb8, 0x11, 0x6e, 0x84, 0xcf, 0x2b, 0x17, 0x34, 0x7e, 0xbc, 0x18, 0x00, 0x18, 0x1c, 0x4e, 0x2a, 0x1f, 0xb8, 0xdd, 0x53, 0xe1, 0xc6, 0x35, 0x51, 0x8c, 0x7d, 0xac, 0x47, 0xe9}
-            },
-
-            new TestData
-            {
-                password = "pass\0word",
-                salt = "sa\0lt",
-                iterationCount = 4096,
-                expected = new byte[] {0x89, 0xb6, 0x9d, 0x05, 0x16, 0xf8, 0x29, 0x89, 0x3c, 0x69, 0x62, 0x26, 0x65, 0x0a, 0x86, 0x87}
-            },
+            new TestData("password", "salt", 1, "Eg+2z/z4syxD5yJSVsT4N6hlSMkszDVICAWYfLcL4Xs="),
+            new TestData("password", "salt", 2, "rk0Mla9rRtMtCt/5KPBt0CowP47zwlHf1uLYWpVHTEM="),
+            new TestData("password", "salt", 4096, "xeR41ZKIyEGqUw22hFxMjZYok6ABzk4RpJY4c6qYE0o="),
+            new TestData("passwordPASSWORDpassword", "saltSALTsaltSALTsaltSALTsaltSALTsalt", 4096, "NIyJ28vTKy8y2BS4EW6EzysXNH68GAAYHE4qH7jdU+HGNVGMfaxH6Q=="),
+            new TestData("pass\0word", "sa\0lt", 4096, "ibadBRb4KYk8aWImZQqGhw=="),
         };
 
         [Test]
@@ -131,17 +70,18 @@ namespace LastPass.Test
             {
                 foreach (var i in testDataSHA1)
                 {
+                    var expected = Convert.FromBase64String(i.Expected);
                     byte[][] results =
                     {
-                        new PBKDF2(hmac, i.password, i.salt, i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.password), i.salt, i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, i.password, UTF8Encoding.UTF8.GetBytes(i.salt), i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.password), UTF8Encoding.UTF8.GetBytes(i.salt), i.iterationCount).GetBytes(i.expected.Length),
+                        new PBKDF2(hmac, i.Password, i.Salt, i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.Password), i.Salt, i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, i.Password, UTF8Encoding.UTF8.GetBytes(i.Salt), i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.Password), UTF8Encoding.UTF8.GetBytes(i.Salt), i.IterationCount).GetBytes(expected.Length),
                     };
 
                     foreach (var j in results)
                     {
-                        Assert.AreEqual(j, i.expected);
+                        Assert.AreEqual(expected, j);
                     }
                 }
             }
@@ -154,17 +94,18 @@ namespace LastPass.Test
             {
                 foreach (var i in testDataSHA256)
                 {
+                    var expected = Convert.FromBase64String(i.Expected);
                     byte[][] results =
                     {
-                        new PBKDF2(hmac, i.password, i.salt, i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.password), i.salt, i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, i.password, UTF8Encoding.UTF8.GetBytes(i.salt), i.iterationCount).GetBytes(i.expected.Length),
-                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.password), UTF8Encoding.UTF8.GetBytes(i.salt), i.iterationCount).GetBytes(i.expected.Length),
+                        new PBKDF2(hmac, i.Password, i.Salt, i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.Password), i.Salt, i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, i.Password, UTF8Encoding.UTF8.GetBytes(i.Salt), i.IterationCount).GetBytes(expected.Length),
+                        new PBKDF2(hmac, UTF8Encoding.UTF8.GetBytes(i.Password), UTF8Encoding.UTF8.GetBytes(i.Salt), i.IterationCount).GetBytes(expected.Length),
                     };
 
                     foreach (var j in results)
                     {
-                        Assert.AreEqual(j, i.expected);
+                        Assert.AreEqual(expected, j);
                     }
                 }
             }
