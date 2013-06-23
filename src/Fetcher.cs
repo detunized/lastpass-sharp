23b3474e51fbfe8e725cb24825d407e040004804
detunized <detunized@gmail.com>
2013-06-23 18:45:22 +0200

    Login to LastPass (no tests so far)

--- src/Fetcher.cs
+++ src/Fetcher.cs
@@ -1,6 +1,40 @@
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
 namespace LastPass
 {
-    class Fetcher
+    public class Fetcher
     {
+        public Fetcher(string username, string password, int iterationCount = 1)
+        {
+            this.username = username;
+            this.password = password;
+            this.iterationCount = iterationCount;
+        }
+
+        public void Login()
+        {
+            using (var web = new WebClient())
+            {
+                var r = web.UploadValues("https://lastpass.com/login.php", new NameValueCollection()
+                    {
+                        {"method", "mobile"},
+                        {"web", "1"},
+                        {"xml", "1"},
+                        {"username", username},
+                        {"hash", FetcherHelper.MakeHash(username, password, iterationCount)},
+                        {"iterations", iterationCount.ToString()}
+                    });
+
+                Console.WriteLine("===\npost: {0} {1}\n===", Encoding.UTF8.GetString(r), web.ResponseHeaders);
+            }
+        }
+
+        private readonly string username;
+        private readonly string password;
+        private int iterationCount;
     }
 }
--- test/FetcherTest.cs
+++ test/FetcherTest.cs
@@ -6,5 +6,13 @@ namespace LastPass.Test
     [TestFixture]
     class FetcherTest
     {
+        [Test]
+        public void Fetch()
+        {
+            // TODO: Mock WebClient and pass and pass it to Fetcher
+            //       Currently this test doesn't really test anything
+            new Fetcher("lastpass.ruby@gmail.com", "&nT%*pMWJb*7s6u1").Login();
+            new Fetcher("lastpass.ruby@gmail.com", "&nT%*pMWJb*7s6u1", 5000).Login();
+        }
     }
 }
