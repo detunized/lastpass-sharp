49e73ac29b2ccf8da7d581a0cc1c4d8cf74562e4
detunized <detunized@gmail.com>
2013-06-28 01:37:29 +0200

    uint.FromBigEndian

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace LastPass
 {
     static class Extensions
@@ -5,6 +7,11 @@ namespace LastPass
         public static uint Reverse(this uint x)
         {
             return ((x & 0xff) << 24) | ((x & 0xff00) << 8) | ((x & 0xff0000) >> 8) | ((x & 0xff000000) >> 24);
+        }
+
+        public static uint FromBigEndian(this uint x)
+        {
+            return BitConverter.IsLittleEndian ? x.Reverse() : x;
         }
     }
 }
--- test/ExtensionsTest.cs
+++ test/ExtensionsTest.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace LastPass.Test
@@ -14,6 +15,14 @@ namespace LastPass.Test
             Assert.AreEqual(0xff00ff00u, 0xff00ffu.Reverse());
             Assert.AreEqual(0x78563412u, 0x12345678u.Reverse());
             Assert.AreEqual(0xdeadbeefu, 0xefbeaddeu.Reverse());
+        }
+
+        [Test]
+        public void FromBigEndian()
+        {
+            Assert.AreEqual(0u, BitConverter.ToUInt32(new byte[] {0x00, 0x00, 0x00, 0x00}, 0).FromBigEndian());
+            Assert.AreEqual(0x12345678u, BitConverter.ToUInt32(new byte[] {0x12, 0x34, 0x56, 0x78}, 0).FromBigEndian());
+            Assert.AreEqual(0xdeadbeefu, BitConverter.ToUInt32(new byte[] {0xde, 0xad, 0xbe, 0xef}, 0).FromBigEndian());
         }
     }
 }
