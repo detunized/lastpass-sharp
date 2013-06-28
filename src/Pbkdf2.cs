774f1e5353534a0e7b2d622c8c3530124d553acd
detunized <detunized@gmail.com>
2013-06-28 20:15:09 +0200

    PBKDF2 is a static class now

--- src/Pbkdf2.cs
+++ src/Pbkdf2.cs
@@ -3,10 +3,8 @@ using System.Security.Cryptography;
 
 namespace LastPass
 {
-    class Pbkdf2
+    static class Pbkdf2
     {
-        // TODO: Don't really need this Microsoft interface, just make it a static function
-
         public static byte[] Generate(string password, string salt, int iterationCount, int byteCount)
         {
             return Generate(password.ToBytes(), salt.ToBytes(), iterationCount, byteCount);
@@ -26,49 +24,30 @@ namespace LastPass
         {
             using (var hmac = new HMACSHA256())
             {
-                return new Pbkdf2(hmac, password, salt, iterationCount).GetBytes(byteCount);
+                hmac.Key = password;
+
+                // TODO: Check for byteCount being too big
+                var bytes = new byte[byteCount];
+                var hashSize = hmac.HashSize / 8;
+                var blockCount = (byteCount + hashSize - 1) / hashSize;
+                for (var i = 0; i < blockCount; ++i)
+                {
+                    // TODO: Calculate the value in-place
+                    var block = CalculateBlock(i + 1, hmac, salt, iterationCount);
+                    var offset = i * hashSize;
+                    var size = Math.Min(hashSize, byteCount - offset);
+                    Array.Copy(block, 0, bytes, offset, size);
+                }
+
+                return bytes;
             }
         }
 
-        private Pbkdf2(HMAC hashFunction, byte[] password, byte[] salt, int iterationCount)
-        {
-            // TODO: Check arguments and throw exceptions
-            HashFunction = hashFunction;
-            Password = password;
-            Salt = salt;
-            IterationCount = iterationCount;
-
-            HashFunction.Key = Password;
-        }
-
-        public byte[] GetBytes(int byteCount)
-        {
-            // TODO: Check for byteCount being too big
-            var bytes = new byte[byteCount];
-            var hashSize = HashFunction.HashSize / 8;
-            var blockCount = (byteCount + hashSize - 1) / hashSize;
-            for (var i = 0; i < blockCount; ++i)
-            {
-                // TODO: Calculate the value in-place
-                var block = CalculateBlock(i + 1);
-                var offset = i * hashSize;
-                var size = Math.Min(hashSize, byteCount - offset);
-                Array.Copy(block, 0, bytes, offset, size);
-            }
-
-            return bytes;
-        }
-
-        public HMAC HashFunction { get; private set; }
-        public byte[] Password { get; private set; }
-        public byte[] Salt { get; private set; }
-        public int IterationCount { get; private set; }
-
-        private byte[] CalculateBlock(int blockIndex)
+        private static byte[] CalculateBlock(int blockIndex, HMAC hmac, byte[] salt, int iterationCount)
         {
             // TODO: Get rid if the temporary
-            var hashInput = new byte[Salt.Length + 4];
-            Salt.CopyTo(hashInput, 0);
+            var hashInput = new byte[salt.Length + 4];
+            salt.CopyTo(hashInput, 0);
 
             // TODO: Get rid if the temporary
             var indexBytes = BitConverter.GetBytes(blockIndex);
@@ -76,13 +55,13 @@ namespace LastPass
             {
                 Array.Reverse(indexBytes);
             }
-            indexBytes.CopyTo(hashInput, Salt.Length);
+            indexBytes.CopyTo(hashInput, salt.Length);
 
-            var hashed = HashFunction.ComputeHash(hashInput);
+            var hashed = hmac.ComputeHash(hashInput);
             var result = hashed;
-            for (var i = 1; i < IterationCount; ++i)
+            for (var i = 1; i < iterationCount; ++i)
             {
-                hashed = HashFunction.ComputeHash(hashed);
+                hashed = hmac.ComputeHash(hashed);
                 for (var j = 0; j < hashed.Length; ++j)
                 {
                     result[j] ^= hashed[j];
