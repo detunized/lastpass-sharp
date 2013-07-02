37ea224ef0a0fe9bde010f5d6e36ab46a02ca579
detunized <detunized@gmail.com>
2013-07-03 01:28:24 +0200

    DRY up a bit

--- test/AccountTest.cs
+++ test/AccountTest.cs
@@ -8,11 +8,16 @@ namespace LastPass.Test
         [Test]
         public void Accout_properties_are_set()
         {
-            var account = new Account("name", "username", "password", "url");
-            Assert.AreEqual("name", account.Name);
-            Assert.AreEqual("username", account.Username);
-            Assert.AreEqual("password", account.Password);
-            Assert.AreEqual("url", account.Url);
+            var name = "name";
+            var username = "username";
+            var password = "password";
+            var url = "url";
+
+            var account = new Account(name, username, password, url);
+            Assert.AreEqual(name, account.Name);
+            Assert.AreEqual(username, account.Username);
+            Assert.AreEqual(password, account.Password);
+            Assert.AreEqual(url, account.Url);
         }
     }
 }
