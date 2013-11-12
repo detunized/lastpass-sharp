0f9446822941f2c6d048647ecd4d781878251a2f
detunized <detunized@gmail.com>
2013-11-12 01:09:05 +0100

    Added copyright headers

--- example/AssemblyInfo.cs
+++ example/AssemblyInfo.cs
@@ -1,5 +1,7 @@
+// Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
--- src/Vault.cs
+++ src/Vault.cs
@@ -1,3 +1,6 @@
+// Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
 using System.Linq;
 
 namespace LastPass
