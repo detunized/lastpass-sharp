d433f97066670abbb5648592e058966bae78dd96
detunized <detunized@gmail.com>
2014-02-19 01:07:22 +0100

    Create decrypted vault

--- src/Vault.cs
+++ src/Vault.cs
@@ -23,6 +23,12 @@ namespace LastPass
             });
         }
 
+        // TODO: Make a test for this!
+        public static Vault Create(Blob blob, string username, string password)
+        {
+            return new Vault(blob, blob.MakeEncryptionKey(username, password));
+        }
+
         public static Blob Download(string username, string password, string multifactorPassword = null)
         {
             return Fetcher.Fetch(Fetcher.Login(username, password, multifactorPassword));
@@ -47,6 +53,23 @@ namespace LastPass
         {
             foreach (var i in Accounts)
                 i.Decrypt(fields, encryptionKey);
+        }
+
+        // TODO: Make a test for this!
+        private Vault(Blob blob, byte[] encryptionKey)
+        {
+            ParserHelper.WithBytes(blob.Bytes, reader => {
+                var chunks = ParserHelper.ExtractChunks(reader);
+                Accounts = chunks.ContainsKey("ACCT")
+                    ? chunks["ACCT"].Select(ParserHelper.ParseAccount).ToArray()
+                    : new Account[] { };
+            });
+
+            DecryptAllAccounts(Account.Field.Name |
+                               Account.Field.Username |
+                               Account.Field.Password |
+                               Account.Field.Group,
+                               encryptionKey);
         }
 
         private Vault(Account[] accounts, int keyIterationCount)
